9db4211a2b03e8a0829d66b4a06c6b80d5b0e93a
leevillion <le.evillion@gmail.com>
2013-08-03 19:20:31 +0800

    添加自动检查发布包csv路径脚本

---
+++ python3.3/工作中用到的脚本/匹配发布包中脚本路径与CSV中的一致性/find_file.py
@@ -0,0 +1,39 @@
+﻿import os
+
+class Searchfile:
+
+    '查找指定盘符下指定文件'
+
+    def __init__(self,file_name):
+        #self.drive=drive
+        self.file_name=file_name
+        self.position=[]  #存放目标文件完全路径
+
+    def search(self,search_dir):
+        '递归查找'
+
+        if os.path.isdir(search_dir):
+            try:
+                filename=os.listdir(search_dir)
+                #d=search_dir
+                for i in filename:
+                    if self.file_name==i:
+                        self.position.append(os.path.join(search_dir,i))
+                        return os.path.join(search_dir,i)
+                        #print(os.path.join(search_dir,i))
+                    else:
+                        self.search(os.path.join(search_dir,i))
+            except:
+                print(os.path.split(search_dir))
+
+
+##c=Searchfile('FA_安装包_20121119')
+##c.search('E:\\')
+
+
+
+
+
+
+
+
---
+++ python3.3/工作中用到的脚本/匹配发布包中脚本路径与CSV中的一致性/match_csv.py
@@ -0,0 +1,68 @@
+﻿import os,csv,re
+from find_file import Searchfile
+
+class Getsqlinfo(Searchfile):
+
+    def __init__(self,file_name):
+        self.scriptfile_name={}
+        self.sql_name={}  #存储脚本文件，键值对
+        #super(Match,self).__init__()
+        Searchfile.__init__(self,file_name)
+
+    def get_filename(self,drive):
+        '获得script子文件内容'
+        self.search(drive)
+        self.file_path=self.position[0]
+
+        for prefilename in os.listdir(self.file_path):
+            if os.path.isdir(os.path.join(self.file_path,prefilename)):
+                self.scriptfile_name[prefilename]=os.listdir(self.file_path+'\\'+prefilename)
+            else:
+                self.scriptfile_name[prefilename]=prefilename
+
+    def get_scriptname(self,drive):
+        self.get_filename(drive)
+        for i in self.scriptfile_name['scripts']:
+            self.sql_name[i]=os.listdir(self.position[0]+'\\'+'scripts'+'\\'+i)
+
+class Getcsvinfo:
+
+    def __init__(self):
+        self.csv=[]
+        self.csv_scripts=[] #存储csv文件中脚本路径值
+        self.count=2
+
+    def get_csv(self,file_path):
+        f=open(file_path)
+        c=csv.reader(f)
+        for line in c:
+            self.csv.append(line)
+        while self.count<len(self.csv)-7:
+            self.csv_scripts.append(self.csv[self.count][4])
+            self.count+=1
+
+
+
+def match():
+
+    M=Getsqlinfo('FA_安装包_20121119')
+    M.get_scriptname('E:\\')
+    N=Getcsvinfo()
+    N.get_csv('E:\\release.csv')
+
+    match_pattern=re.compile(r'.*\\(.*)\\(.*\.[a-z]{3})') #生成匹配模式对象
+    for member in N.csv_scripts:
+        match_result=match_pattern.match(member)
+        if match_result:
+            if match_result.group(1) in M.sql_name and match_result.group(2) in M.sql_name[match_result.group(1)]:
+                pass
+            else:
+                print(match_result.group(1)+'\\'+match_result.group(2))
+
+if __name__=='__main__':
+    match()
+
+
+
+
+
